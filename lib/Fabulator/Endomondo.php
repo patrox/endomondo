379320418dac2b674f4d7f8aeba743a4735a96bf
Michal Ozogán <michal@ozogan.eu>
2015-08-08 10:22:27 +0200

    Add workout id const

--- lib/Fabulator/Endomondo.php
+++ lib/Fabulator/Endomondo.php
@@ -8,6 +8,70 @@ use GuzzleHttp\Client;
 
 class Endomondo
 {
+    const SPORT_RUNNING = 0;
+    const SPORT_CYCLING_TRANSPORT = 1;
+    const SPORT_CYCLING_SPORT = 2;
+    const SPORT_MOUNTAIN_BIKINGS = 3;
+    const SPORT_SKATING = 4;
+    const SPORT_ROLLER_SKIING = 5;
+    const SPORT_SKIING_CROSS_COUNTRY = 6;
+    const SPORT_SKIING_DOWNHILL = 7;
+    const SPORT_SNOWBOARDING = 8;
+    const SPORT_KAYAKING = 9;
+    const SPORT_KITE_SURFING = 10;
+    const SPORT_ROWING = 11;
+    const SPORT_SAILING = 12;
+    const SPORT_WINDSURFING = 13;
+    const SPORT_FINTESS_WALKING = 14;
+    const SPORT_GOLFING = 15;
+    const SPORT_HIKING = 16;
+    const SPORT_ORIENTEERING = 17;
+    const SPORT_WALKING = 18;
+    const SPORT_RIDING = 19;
+    const SPORT_SWIMMING = 20;
+    const SPORT_SPINNING = 21;
+    const SPORT_OTHER = 22;
+    const SPORT_AEROBICS = 23;
+    const SPORT_BADMINTON = 24;
+    const SPORT_BASEBALL = 25;
+    const SPORT_BASKETBALL = 26;
+    const SPORT_BOXING = 27;
+    const SPORT_CLIMBING_STAIRS = 28;
+    const SPORT_CRICKET = 29;
+    const SPORT_CROSS_TRAINING = 30;
+    const SPORT_DANCING = 31;
+    const SPORT_FENCING = 32;
+    const SPORT_FOOTBALL_AMERICAN = 33;
+    const SPORT_FOOTBALL_RUGBY = 34;
+    const SPORT_FOOTBALL_SOCCER = 35;
+    const SPORT_HANDBALL = 36;
+    const SPORT_HOCKEY = 37;
+    const SPORT_PILATES = 38;
+    const SPORT_POLO = 39;
+    const SPORT_SCUBA_DIVING = 40;
+    const SPORT_SQUASH = 41;
+    const SPORT_TABLE_TENIS = 42;
+    const SPORT_TENNIS = 43;
+    const SPORT_VOLEYBALL_BEACH = 44;
+    const SPORT_VOLEYBALL_INDOOR = 45;
+    const SPORT_WEIGHT_TRAINING = 46;
+    const SPORT_YOGA = 47;
+    const SPORT_MARTINAL_ARTS = 48;
+    const SPORT_GYMNASTICS = 49;
+    const SPORT_STEP_COUNTER = 50;
+    const SPORT_CIRKUIT_TRAINING = 87;
+
+    // Authentication url. Special case.
+    const URL_AUTH = '/mobile/auth';
+
+    const URL_WORKOUTS = '/mobile/api/workout/list';
+    const URL_WORKOUT = '/mobile/api/workout/get';
+    const URL_WORKOUT_POST  = '/mobile/api/workout/post';
+    const URL_WORKOUT_CREATE  = '/mobile/track';
+
+    const URL_PROFILE_GET = '/mobile/api/profile/account/get';
+    const URL_PROFILE_POST = '/mobile/api/profile/account/post';
+
     private $country = "EN";
     private $deviceId = null;
     private $os = "Android";
@@ -22,17 +86,6 @@ class Endomondo
     private $profile = null;
 
     private $workoutFactory = null;
-
-    // Authentication url. Special case.
-    const URL_AUTH = '/mobile/auth';
-
-    const URL_WORKOUTS = '/mobile/api/workout/list';
-    const URL_WORKOUT = '/mobile/api/workout/get';
-    const URL_WORKOUT_POST  = '/mobile/api/workout/post';
-    const URL_WORKOUT_CREATE  = '/mobile/track';
-
-    const URL_PROFILE_GET = '/mobile/api/profile/account/get';
-    const URL_PROFILE_POST = '/mobile/api/profile/account/post';
 
     public function __construct()
     {
